a5012d29ed8817aacfa167e05b7a751f252df828
Kaiky <kaikyborges60@gmail.com>
2026-09-03 16:27:14 -0300

    test: modify app entrypoint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
 from app.routers import ativos, usuarios,transacoes
 from fastapi import FastAPI
-from app.database import engine, Base
-from app import db_models
 
 #Base.metadata.create_all(bind=engine)
 
+#Rotas importantes
 app = FastAPI()
 app.include_router(ativos.router)
 app.include_router(usuarios.router)
